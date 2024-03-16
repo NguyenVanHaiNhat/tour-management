7dac87c539e13adc939bceb388d6695edb16b838
thanhdung26031994 <thanhdung26031994@gmail.com>
2024-03-16 23:47:00 +0700

    update Pagination

--- src/main/java/org/example/tourmanagement/service/GeneralService.java
+++ src/main/java/org/example/tourmanagement/service/GeneralService.java
@@ -1,9 +1,12 @@
 package org.example.tourmanagement.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 
 public interface GeneralService <T>{
-    Iterable<T> findAll();
+    Page<T> findAll(Pageable pageable);
     Optional<T> findById(Long id);
     T save(T t);
     void remove(Long id);
