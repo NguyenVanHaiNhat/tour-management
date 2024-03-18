ed1f8c49ef6266d2d22352f1f8053794bd781f2d
nhat0120230 <nhatnguyen22102003@gmail.com>
2024-03-18 15:19:59 +0700

    done authen and author using Jwt

--- src/main/java/org/example/tourmanagement/config/SecurityConfig.java
+++ src/main/java/org/example/tourmanagement/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package org.example.tourmanagement.config;
 
-import org.example.tourmanagement.service.impl.UserService;
+import org.example.tourmanagement.config.jwt.CustomAccessDeniedHandler;
+import org.example.tourmanagement.config.jwt.JwtAuthenticationTokenFilter;
+import org.example.tourmanagement.config.jwt.RestAuthenticationEntryPoint;
+import org.example.tourmanagement.config.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,6 +44,7 @@ public class SecurityConfig {
         return http.build();
     }
 
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(10);
@@ -49,5 +56,24 @@ public class SecurityConfig {
         authenticationProvider.setUserDetailsService(userService);
         authenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return authenticationProvider;
+    }
+    @Bean
+    public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter(){
+        return new JwtAuthenticationTokenFilter();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public RestAuthenticationEntryPoint restAuthenticationEntryPoint(){
+        return new RestAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public CustomAccessDeniedHandler customAccessDeniedHandler(){
+        return new CustomAccessDeniedHandler();
     }
 }
--- src/main/java/org/example/tourmanagement/model/Role.java
+++ src/main/java/org/example/tourmanagement/model/Role.java
@@ -5,17 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
 @Entity
 @Data
-public class Role implements GrantedAuthority {
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @Override
-    public String getAuthority() {
-        return name;
-    }
 }
